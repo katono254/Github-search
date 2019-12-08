5f99160d1d975ab1ff2e5fb0fcc871069fc4284b
jakesIII <jayruma@yahoo.com>
2019-12-08 14:35:36 +0300

    Add an environment and a constant path to the github service

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-
+import{AppConst} from './Constants/constant'
+import{environment} from '../environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
+  private serverPath: string = AppConst.serverPath;
+  private sPath: string = AppConst.Path;
+
 
   constructor() { }
 }
