7e51f11f7f0cc6e61f49fd489f6fd57df1a15c55
jakesIII <jayruma@yahoo.com>
2019-12-08 13:31:01 +0300

    Add components through routing method

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
+import { GitPageComponent } from './git-page/git-page.component';
+import{SearchComponent} from './search/search.component';
+
+
+
+
 const routes: Routes = [];
 
 @NgModule({
